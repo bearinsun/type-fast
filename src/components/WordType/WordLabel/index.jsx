1427afc8813b1cf64eddf8b8efe3b98a239f1745
Spencer Berenson <spencer@berenson.me>
2018-08-09 10:36:32 -0400

    Separate marking logic into computePartials function
    Previously, every wrong and right letter would individually reside in
    its own mark element. Now, computePartials determines when letters
    deserve the same mark element.

--- src/components/WordType/WordLabel/index.jsx
+++ src/components/WordType/WordLabel/index.jsx
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 
 export default class WordLabel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			letters: this.props.word.split("")
+		};
+	}
+
+	computePartials() {
+		return this.props.word
+			.split("")
+			.reduce((previousPartials, letter, index) => {
+				let lastPartial = previousPartials[previousPartials.length - 1];
+				const isTyped = this.props.typedWord[index] !== undefined,
+					isCorrect =
+						this.props.word[0] === this.props.typedWord[0] &&
+						letter === this.props.typedWord[index];
+
+				if (
+					!lastPartial ||
+					lastPartial.isTyped !== isTyped ||
+					lastPartial.isCorrect !== isCorrect
+				) {
+					lastPartial = {
+						letters: "",
+						isTyped: this.props.typedWord[index] !== undefined
+					};
+					previousPartials.push(lastPartial);
+				}
+
+				lastPartial.letters += letter;
+
+				lastPartial.isCorrect = isCorrect;
+
+				return previousPartials;
+			}, []);
+	}
+
 	render() {
 		return (
-			<label className="word-input__label" htmlFor={this.props.htmlFor}>
-				{this.props.word.split("").map((letter, index) => {
-					if (!this.props.typedWord[index]) return letter;
+			<label className="word-type__label" htmlFor={this.props.htmlFor}>
+				{this.computePartials().map(partial => {
+					if (!partial.isTyped) return partial.letters;
 
-					let className = "word-input__character ";
-					if (
-						this.props.word[0] === this.props.typedWord[0] &&
-						letter === this.props.typedWord[index]
-					)
-						className += "word-input__character--right";
-					else className += "word-input__character--wrong";
+					let className = "word-type__letters word-type__letters--";
+					className += partial.isCorrect ? "correct" : "incorrect";
 
-					return <mark className={className}>{letter}</mark>;
+					return <mark className={className}>{partial.letters}</mark>;
 				})}
 			</label>
 		);
