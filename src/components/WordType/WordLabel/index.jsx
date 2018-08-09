b56c1b75c0f8101e464ea03a0f7c07d99e264dc2
Spencer Berenson <spencer@berenson.me>
2018-08-09 13:45:13 -0400

    Convert WordLabel to a dumb component

--- src/components/WordType/WordLabel/index.jsx
+++ src/components/WordType/WordLabel/index.jsx
@@ -1,59 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class WordLabel extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			letters: this.props.word.split("")
-		};
+export default function WordLabel(props) {
+	function computePartials() {
+		const typedLetters = (props.typedWord.length > props.word.length
+			? props.typedWord.slice(0, props.word.length)
+			: props.typedWord
+		).split("");
+
+		return typedLetters.reduce((previousPartials, letter, index) => {
+			let lastPartial = previousPartials[previousPartials.length - 1];
+			const isCorrect = letter === props.word[index];
+
+			if (!lastPartial || lastPartial.isCorrect !== isCorrect) {
+				lastPartial = {
+					letters: ""
+				};
+				previousPartials.push(lastPartial);
+			}
+
+			lastPartial.letters += props.word[index];
+			lastPartial.isCorrect = isCorrect;
+
+			return previousPartials;
+		}, []);
 	}
 
-	computePartials() {
-		return this.props.word
-			.split("")
-			.reduce((previousPartials, letter, index) => {
-				let lastPartial = previousPartials[previousPartials.length - 1];
-				const isTyped = this.props.typedWord[index] !== undefined,
-					isCorrect =
-						this.props.word[0] === this.props.typedWord[0] &&
-						letter === this.props.typedWord[index];
+	return (
+		<label className="word-type__label" htmlFor={props.htmlFor}>
+			{props.typedWord
+				? [
+						computePartials().map((partial, index) => {
+							let className =
+								"word-type__letters word-type__letters--";
+							className += partial.isCorrect
+								? "correct"
+								: "incorrect";
 
-				if (
-					!lastPartial ||
-					lastPartial.isTyped !== isTyped ||
-					lastPartial.isCorrect !== isCorrect
-				) {
-					lastPartial = {
-						letters: "",
-						isTyped: this.props.typedWord[index] !== undefined
-					};
-					previousPartials.push(lastPartial);
-				}
-
-				lastPartial.letters += letter;
-
-				lastPartial.isCorrect = isCorrect;
-
-				return previousPartials;
-			}, []);
-	}
-
-	render() {
-		return (
-			<label className="word-type__label" htmlFor={this.props.htmlFor}>
-				{this.computePartials().map((partial, index) => {
-					if (!partial.isTyped) return partial.letters;
-
-					let className = "word-type__letters word-type__letters--";
-					className += partial.isCorrect ? "correct" : "incorrect";
-
-					return (
-						<mark className={className} key={index}>
-							{partial.letters}
-						</mark>
-					);
-				})}
-			</label>
-		);
-	}
+							return (
+								<mark className={className} key={index}>
+									{partial.letters}
+								</mark>
+							);
+						}),
+						props.word.slice(props.typedWord.length)
+				  ]
+				: props.word}
+		</label>
+	);
 }
