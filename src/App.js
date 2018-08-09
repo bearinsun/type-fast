13c7f6f095f3c503544961e8de9b3046210a8aa3
Spencer Berenson <spencer@berenson.me>
2018-08-08 20:07:41 -0400

    Replace original experimentation code with WordInput instance

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
+import "normalize.css/normalize.css";
 import "./App.css";
+
+import WordInput from "./components/WordInput";
 
 class App extends Component {
 	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to
-					reload.
-				</p>
-			</div>
-		);
+		return <WordInput id="control" />;
 	}
 }
 
