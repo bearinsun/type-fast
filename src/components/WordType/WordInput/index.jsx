2db3e035b2f3801a42c0fd83fcdbcb5c0a869ae9
Spencer Berenson <spencer@berenson.me>
2018-08-09 10:31:09 -0400

    Move WordInput's change event handler into its own function

--- src/components/WordType/WordInput/index.jsx
+++ src/components/WordType/WordInput/index.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 
 export default class WordInput extends Component {
+	handleChange(event) {
+		const value = event.target.value;
+		if (value !== this.props.word) {
+			this.props.updateTypedWord(value);
+			return;
+		}
+
+		this.props.changeWord();
+	}
+
 	render() {
 		return (
 			<input
 				id={this.props.id}
 				className="word-type__input"
 				value={this.props.typedWord}
-				onChange={event => {
-					const value = event.target.value;
-					if (value !== this.props.word) {
-						this.props.updateTypedWord(value);
-						return;
-					}
-
-					this.props.completeWord();
-				}}
+				onChange={this.handleChange.bind(this)}
 				autoFocus
 			/>
 		);
