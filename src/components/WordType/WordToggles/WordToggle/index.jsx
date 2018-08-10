0b18b8055e94b1e42b4737d58f73bd9434b1ad1e
Spencer Berenson <spencer@berenson.me>
2018-08-10 08:39:36 -0400

    Wrap WordToggle elements in div container

--- src/components/WordType/WordToggles/WordToggle/index.jsx
+++ src/components/WordType/WordToggles/WordToggle/index.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import classNames from "classnames";
 
 export default function WordToggle(props) {
-	return [
-		<input
-			id={props.id}
-			className={classNames("word-type__checkbox")}
-			checked={props.checked}
-			onChange={props.onChange}
-			type="checkbox"
-		/>,
-		<label className={classNames("word-type__label")} htmlFor={props.id}>
-			{props.label}
-		</label>
-	];
+	return (
+		<div>
+			<input
+				id={props.id}
+				className={classNames("word-type__checkbox")}
+				checked={props.checked}
+				onChange={props.onChange}
+				type="checkbox"
+			/>
+			<label
+				className={classNames("word-type__label")}
+				htmlFor={props.id}
+			>
+				{props.label}
+			</label>
+		</div>
+	);
 }
