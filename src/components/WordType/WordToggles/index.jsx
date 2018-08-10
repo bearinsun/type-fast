0a4a1eb767b3e4f97b81fb11b440fa8f95377602
Spencer Berenson <spencer@berenson.me>
2018-08-10 08:39:18 -0400

    Add keys to children of WordToggles

--- src/components/WordType/WordToggles/index.jsx
+++ src/components/WordType/WordToggles/index.jsx
@@ -5,8 +5,10 @@ import "./index.css";
 export default function WordToggles(props) {
 	return (
 		<ul className={classNames("word-type__toggles")}>
-			{props.children.map(element => (
-				<li className={classNames("word-type__toggle")}>{element}</li>
+			{props.children.map((element, index) => (
+				<li className={classNames("word-type__toggle")} key={index}>
+					{element}
+				</li>
 			))}
 		</ul>
 	);
